c70a7a3ed01d679ba900e56c073dcfa2e2dc227d
Lorenzo <lorenzozippilli@hotmail.com>
2023-02-01 00:35:19 -0300

    ConnectionString -> SistemaGestionConnectionString

--- FinalCoder.Core/Globals.cs
+++ FinalCoder.Core/Globals.cs
@@ -4,10 +4,11 @@ namespace FinalCoder.Core
 {
     public static class Globals
     {
-        public static string _ConnectionString => "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SistemaGestion;Integrated Security=True";
+        public static string SistemaGestionConnectionString => "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SistemaGestion;Integrated Security=True";
+
         public static SqlConnection SqlConnection => new SqlConnection()
         {
-            ConnectionString = _ConnectionString
+            ConnectionString = SistemaGestionConnectionString
         };
     }
 }
