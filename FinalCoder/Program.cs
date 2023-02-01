8863c308c303796bbe279b770dca9326033d4e75
Lorenzo <lorenzozippilli@hotmail.com>
2023-02-01 00:06:32 -0300

    Agregadas anotaciones

--- FinalCoder/Program.cs
+++ FinalCoder/Program.cs
@@ -1,35 +1,49 @@
 ﻿using FinalCoder.Core.Models;
 using FinalCoder.Core.Repositories;
 
-//Product remeraMangaLarga = ProductRepository.GetById(1);
-//Console.WriteLine(remeraMangaLarga.Description);
+/* Obtener un PRODUCTO por su ID */
+Product remeraMangaLarga = ProductRepository.GetById(1);
+Console.WriteLine(remeraMangaLarga.Description);
+/* ============================================================== */
 
-//Product buzo = ProductRepository.GetByDescription("Buzo");
-//Console.WriteLine(buzo.Description);
+/* Obtener un PRODUCTO por su Nombre/Descripcion */
+Product buzo = ProductRepository.GetByDescription("Buzo");
+Console.WriteLine(buzo.Description);
+/* ============================================================== */
 
-//IEnumerable<Product> productos = ProductRepository.GetAll();
-//foreach (var p in productos)
-//{
-//    Console.WriteLine(p.Description);
-//}
+/* Listar todos los registros de la tabla Producto */
+IEnumerable<Product> productos = ProductRepository.GetAll();
+foreach (var p in productos)
+{
+    Console.WriteLine(p.Description);
+}
+/* ============================================================== */
 
-//Product gorra = new Product
-//{
-//    Description = "Gorra",
-//    Cost = 75,
-//    SellPrice = 110,
-//    Stock = 20,
-//    UserId = 1,
-//};
-//int modificaciones = ProductRepository.Insert(gorra);
+/* INSERTAR un Producto */
+Product gorra = new Product
+{
+    Description = "Gorra",
+    Cost = 75,
+    SellPrice = 110,
+    Stock = 20,
+    UserId = 1,
+};
+int modificaciones = ProductRepository.Insert(gorra);
 
-//var resultado = ProductRepository.GetByDescription(gorra.Description);
-//Console.WriteLine($"{resultado.ID} {resultado.Description}");
+var resultado = ProductRepository.GetByDescription(gorra.Description);
+Console.WriteLine($"{resultado.ID} {resultado.Description}");
+/* ============================================================== */
 
-//var gorra = ProductRepository.GetByDescription("Gorra");
-//Console.WriteLine($"{gorra.ID} {gorra.Description}");
-
-gorra.Description = "Gorra roja";
-
-ProductRepository.Update(gorra);
+/* ACTUALIZAR un Producto */
+var _gorra = ProductRepository.GetByDescription("Gorra");
 Console.WriteLine($"{gorra.ID} {gorra.Description}");
+
+_gorra.Description = "Gorra roja";
+ProductRepository.Update(gorra);
+
+Console.WriteLine($"{gorra.ID} {gorra.Description}");
+/* ============================================================== */
+
+/* ELIMINAR un Producto */
+ProductRepository.Delete(gorra);
+/* ============================================================== */
