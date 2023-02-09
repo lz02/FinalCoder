d077fd65f086c51f9bc61bb458f0bcd4bdf48885
Lorenzo Zippilli <lorenzozippilli@hotmail.com>
2023-02-09 20:40:41 -0300

    Agregado metodo Insert, GetById y GetAllItemsForSale

--- FinalCoder.Core/Repositories/ProductSalesRepository.cs
+++ FinalCoder.Core/Repositories/ProductSalesRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using FinalCoder.Core.Models;
 
@@ -19,12 +20,74 @@ namespace FinalCoder.Core.Repositories
             return productSale;
         }
 
+        public static long Insert(ProductSale productSale)
+        {
+            using (var con = Globals.SqlConnection)
+            {
+                SqlCommand command = new SqlCommand(
+                    $"INSERT INTO {TableName} (Stock, IdProducto, IdVenta) " +
+                    $"VALUES (@stock, @product, @sale) " +
+                    $"SELECT @@IDENTITY", con);
+
+                command.Parameters.AddWithValue("@stock", productSale.Stock);
+                command.Parameters.AddWithValue("@product", productSale.ProductId);
+                command.Parameters.AddWithValue("@sale", productSale.SaleId);
+
+                con.Open();
+                return Convert.ToInt64(command.ExecuteScalar());
+            }
+        }
+
+        public static ProductSale GetById(long id)
+        {
+            using (var con = Globals.SqlConnection)
+            {
+                SqlCommand command = new SqlCommand(
+                    $"SELECT * FROM ProductoVendido " +
+                    $"WHERE Id = @id", con);
+                command.Parameters.AddWithValue("@id", id);
+
+                con.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    return MapToModel(reader);
+                }
+            }
+        }
         /// <summary>
-        /// Obtiene una liista con el detalle de las ventas realizadas por un usuario especifico.
+        /// Obtiene una liista con el detalle de una venta en particular.
+        /// </summary>
+        /// <param name="saleId">ID de la venta.</param>
+        /// <returns>Una liista con el detalle de una venta en particular.</returns>
+        public static IEnumerable<ProductSale> GetAllItemsOfSale(long saleId)
+        {
+            using (var con = Globals.SqlConnection)
+            {
+                SqlCommand command = new SqlCommand(
+                    $"SELECT * FROM ProductoVendido " +
+                    $"INNER JOIN Venta " +
+                    $"ON Venta.Id = ProductoVendido.IdVenta " +
+                    $"WHERE Venta.Id = @id", con);
+                command.Parameters.AddWithValue("@id", saleId);
+
+                con.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    List<ProductSale> productSales = new List<ProductSale>();
+                    while (reader.Read())
+                    {
+                        productSales.Add(MapToModel(reader));
+                    }
+                    return productSales;
+                }
+            }
+        }
+        /// <summary>
+        /// Obtiene una liista con el detalle de TODAS las ventas realizadas por un usuario especifico.
         /// </summary>
         /// <param name="usarId">ID del usuario a buscar.</param>
         /// <returns>Una liista con el detalle de las ventas realizadas por un usuario especifico.</returns>
-        public static IEnumerable<ProductSale> GetAllSalesByUser(long usarId)
+        public static IEnumerable<ProductSale> GetAllByUser(long usarId)
         {
             using (var con = Globals.SqlConnection)
             {
